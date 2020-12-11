1db26b6b8e962db2ae283ace2eca760348a17569
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-11 14:08:26 -0700

    Progress on game files

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+class Game
+
+  def menu_prompt
+    "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+  end
+
+end
---
+++ test/game_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def test_it_exists
+    game = Game.new
+    assert_instance_of Game, game
+  end
+
+  def test_displays_main_menu
+    game = Game.new
+    expected = "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+    assert_equal expected, game.menu_prompt
+  end
+end
