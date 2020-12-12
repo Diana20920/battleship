9576605806f3deebf80e7fc1c98816d1b64e0571
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-12 11:17:57 -0700

    Create player place methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,77 @@
 class Game
 
-  def menu_prompt
-    "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+  def initialize(comp_board, player_board)
+    @comp_board = comp_board
+    @player_board = player_board
   end
+
+  def menu_prompt
+    puts "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit."
+    @menu_selection = gets.chomp
+  end
+
+  def create_ships
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @comp_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @player_submarine = Ship.new("Submarine", 2)
+  end
+
+
+  def play
+    if @menu_selection == "p"
+      display_boards
+    elsif @menu_selection == "q"
+      puts "Quitter"
+    end
+  end
+
+  def display_comp_board
+    puts @comp_board.render
+  end
+
+  def display_player_board
+    puts @player_board.render
+  end
+
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    display_comp_board
+    puts "==============PLAYER BOARD=============="
+    display_player_board
+  end
+
+  def player_cruiser_coords
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    @p_cruiser_coords = gets.chomp
+    @p_cruiser_coords.split(" ").to_a
+  end
+
+  def player_submarine_coords
+    puts "Enter the squares for the Submarine (2 spaces):"
+    @p_submarine_coords = gets.chomp
+    @p_submarine_coords.split(" ")
+  end
+
+  def player_place_cruiser
+    create_ships
+    player_cruiser_coords
+    @player_board.place(@player_cruiser, @p_cruiser_coords.split(" ").to_a)
+  end
+
+  def player_place_submarine
+    create_ships
+    player_submarine_coords
+    @player_board.place(@player_submarine, @p_submarine_coords.split(" ").to_a)
+  end 
+
+  #   player_submarine_coords
+  #   @player_board.place(@player_sub, @p_submarine_coordinate_array)
+  # end
+
+
+
+  # def computer_setup_board
+
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -7,14 +7,28 @@ require './lib/game'
 
 class GameTest < Minitest::Test
 
-  def test_it_exists
-    game = Game.new
-    assert_instance_of Game, game
+  # def test_it_exists
+  #   game = Game.new
+  #   assert_instance_of Game, game
+  # end
+  #
+  # def test_displays_main_menu
+  #   game = Game.new
+  #   expected = "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+  #   assert_equal expected, game.menu_prompt
+  # end
+
+  def test_player_places_cruiser
+    player_board = Board.new
+    comp_board = Board.new
+    game = Game.new(comp_board, player_board)
+    # player_cruiser = Ship.new("Cruiser", 3)
+    # p_cruiser_coordinate_array = ["A1", "A2", "A3"]
+    game.player_place_cruiser
+    expected = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+
+    assert_equal expected, player_board.render(true)
   end
 
-  def test_displays_main_menu
-    game = Game.new
-    expected = "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
-    assert_equal expected, game.menu_prompt
-  end
+
 end
