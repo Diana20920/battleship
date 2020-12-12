6a7b4f74fca5c6b5ea97cedb482f2a19afbaacbd
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-12 15:16:22 -0700

    Create player placement method suite

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ class Game
     @player_cruiser = Ship.new("Cruiser", 3)
     @comp_cruiser = Ship.new("Cruiser", 3)
     @player_submarine = Ship.new("Submarine", 2)
-    @player_submarine = Ship.new("Submarine", 2)
+    @comp_submarine = Ship.new("Submarine", 2)
   end
 
 
@@ -41,37 +41,96 @@ class Game
     display_player_board
   end
 
+  ####PLAYER CRUISER PLACEMENT
+
   def player_cruiser_coords
+    create_ships
     puts "Enter the squares for the Cruiser (3 spaces):"
     @p_cruiser_coords = gets.chomp
     @p_cruiser_coords.split(" ").to_a
   end
 
-  def player_submarine_coords
-    puts "Enter the squares for the Submarine (2 spaces):"
-    @p_submarine_coords = gets.chomp
-    @p_submarine_coords.split(" ")
+  def player_cruiser_invalid_coords
+    puts "Those are invalid coordinates. Please try again:"
+    @p_cruiser_coords = gets.chomp
+    @p_cruiser_coords.split(" ").to_a
+    player_ok_to_place_cruiser_redo
+  end
+
+  def player_ok_to_place_cruiser
+    player_cruiser_coords
+    if @player_board.valid_placement?(@player_cruiser, @p_cruiser_coords.split(" ").to_a) == true
+      player_place_cruiser
+    else
+      player_cruiser_invalid_coords
+    end
+  end
+
+  def player_cruiser_invalid_coords
+    puts "Those are invalid coordinates. Please try again:"
+    @p_cruiser_coords = gets.chomp
+    @p_cruiser_coords.split(" ").to_a
+    player_ok_to_place_cruiser_redo
+  end
+
+  def player_ok_to_place_cruiser_redo
+    if @player_board.valid_placement?(@player_cruiser, @p_cruiser_coords.split(" ").to_a) == true
+      player_place_cruiser
+    else
+      player_cruiser_invalid_coords
+    end
   end
 
   def player_place_cruiser
-    create_ships
-    player_cruiser_coords
     @player_board.place(@player_cruiser, @p_cruiser_coords.split(" ").to_a)
   end
 
-  def player_place_submarine
+  ####### PLAYER SUBMARINE PLACEMENT#####
+
+  def player_submarine_coords
     create_ships
+    puts "Enter the squares for the Submarine (2 spaces):"
+    @p_submarine_coords = gets.chomp
+    @p_submarine_coords.split(" ").to_a
+  end
+
+  def player_submarine_invalid_coords
+    puts "Those are invalid coordinates. Please try again:"
+    @p_submarine_coords = gets.chomp
+    @p_submarine_coords.split(" ").to_a
+    player_ok_to_place_submarine_redo
+  end
+
+  def player_ok_to_place_submarine
     player_submarine_coords
+    if @player_board.valid_placement?(@player_submarine, @p_submarine_coords.split(" ").to_a) == true
+      player_place_submarine
+    else
+      player_submarine_invalid_coords
+    end
+  end
+
+  def player_submarine_invalid_coords
+    puts "Those are invalid coordinates. Please try again:"
+    @p_submarine_coords = gets.chomp
+    @p_submarine_coords.split(" ").to_a
+    player_ok_to_place_submarine_redo
+  end
+
+  def player_ok_to_place_submarine_redo
+    if @player_board.valid_placement?(@player_submarine, @p_submarine_coords.split(" ").to_a) == true
+      player_place_submarine
+    else
+      player_submarine_invalid_coords
+    end
+  end
+
+  def player_place_submarine
     @player_board.place(@player_submarine, @p_submarine_coords.split(" ").to_a)
-  end 
-
-  #   player_submarine_coords
-  #   @player_board.place(@player_sub, @p_submarine_coordinate_array)
-  # end
+  end
 
 
 
-  # def computer_setup_board
 
 
 end
